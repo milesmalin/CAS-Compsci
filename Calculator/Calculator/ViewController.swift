48aafcac452d850e1bf757a84a03c7d1911f2d2f
Miles <milesmalin21@k12.xrds.org>
2019-10-08 10:00:34 -0700

    calc

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -24,7 +24,44 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func didPressEquals(_ sender: Any) {
+        guard let labelInt:Int = Int(labelString)else {
+            return
+        }
+        
+        if(currentMode == .not_set || lastButtonWasMode){
+            return
+        }
+        
+        if(currentMode == .addition){
+            savedNum += labelInt
+        }
+        else if (currentMode == .subtraction){
+            savedNum -= labelInt
+        }
+        currentMode = .not_set
+        labelString = "\(savedNum)"
+        updateText()
+        lastButtonWasMode = true
+    }
+    
+    @IBAction func didPressPlus(_ sender: Any) {
+        changeMode(newMode: .addition)
+    }
+    
+    @IBAction func didPressSubtract(_ sender: Any) {
+        changeMode(newMode: .subtraction)
+    }
+    
+    @IBAction func didPressClear(_ sender: Any) {
+        labelString = "0"
+        currentMode = .not_set
+        savedNum = 0
+        lastButtonWasMode = false
+        label.text = "0"
+    }
+    
     @IBAction func didPressButton(_ sender: UIButton) {
         let stringValue:String? = sender.titleLabel?.text
         
@@ -39,7 +76,17 @@ class ViewController: UIViewController {
         if(currentMode == .not_set){
             savedNum = labelInt
         }
-        
+        label.text = "\(labelInt)"
+    }
+    
+    
+    
+    func changeMode(newMode:modes){
+        if(savedNum == 0){
+            return
+        }
+        currentMode = newMode
+        lastButtonWasMode = true
     }
     
 }

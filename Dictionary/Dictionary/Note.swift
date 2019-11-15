a0b23231811e0bbbcb4bec2466cc4582efe06f7c
Miles <milesmalin21@k12.xrds.org>
2019-11-15 10:00:27 -0800

    dictionary

---
+++ Dictionary/Dictionary/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  Dictionary
+//
+//  Created by Miles Malin on 11/15/19.
+//  Copyright © 2019 Miles Malin. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    var note:String = ""
+    var date:Date = Date()
+}

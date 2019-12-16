27ac48e51b3c06fb4412d25bb4688a1be8bfa93b
Miles <milesmalin21@k12.xrds.org>
2019-12-16 08:58:23 -0800

    Swapi
    Star wars

--- swapiapp/swapiapp/ViewController.swift
+++ swapiapp/swapiapp/ViewController.swift
@@ -16,24 +16,73 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     @IBAction func pressOne(_ sender: Any) {
-       let url: URL = URL(string: "https://swapi.co/api/people/1/")!
+        let url: URL = URL(string: "https://swapi.co/api/films/4/")!
         let responseData: Data? = try? Data(contentsOf: url)
-        if let responseData = responseData{
-        let json: Any? = try?
-            JSONSerialization.jsonObject(with: responseData, options: [])
-            if let json = json{
-        let dictionary: [String: Any]? = json as? [String: Any]
-                if let dictionary = dictionary{
-        let name: String? = dictionary["name"] as? String
-                    if let name = name{
-        nameLabel.text = name
+        if let responseData = responseData {
+            let json: Any? = try? JSONSerialization.jsonObject(with: responseData, options: [])
+            if let json = json {
+                let dictionary: [String: Any]? = json as? [String: Any]
+                if let dictionary = dictionary {
+                    let title: String? = dictionary["title"] as? String
+                    let director: String? = dictionary["director"] as? String
+                    let release_date: String? = dictionary["release_date"] as? String
+                    if let title2:String = title,
+                        let director2:String = director,
+                        let release_date2:String = release_date {
+                        nameLabel.text = "The Star wars film \(title2), was directed by \(director2) and came out on \(release_date2)."
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    @IBAction func pressTwo(_ sender: Any) {
+        let url: URL = URL(string: "https://swapi.co/api/films/5/")!
+        let responseData: Data? = try? Data(contentsOf: url)
+        if let responseData = responseData {
+            let json: Any? = try? JSONSerialization.jsonObject(with: responseData, options: [])
+            if let json = json {
+                let dictionary: [String: Any]? = json as? [String: Any]
+                if let dictionary = dictionary {
+                    let title: String? = dictionary["title"] as? String
+                    let director: String? = dictionary["director"] as? String
+                    let release_date: String? = dictionary["release_date"] as? String
+                    if let title2:String = title,
+                        let director2:String = director, let release_date2:String = release_date {
+                        nameLabel.text = "The Star wars film \(title2), was directed by \(director2) and came out on \(release_date2)."
+                    }
+                    
+                }
+                
+            }
+            
+        }
     }
     
-}
-
-}
-}
-}
+    @IBAction func pressThree(_ sender: Any) {
+        let url: URL = URL(string: "https://swapi.co/api/films/6/")!
+        let responseData: Data? = try? Data(contentsOf: url)
+        if let responseData = responseData {
+            let json: Any? = try? JSONSerialization.jsonObject(with: responseData, options: [])
+            if let json = json {
+                let dictionary: [String: Any]? = json as? [String: Any]
+                if let dictionary = dictionary {
+                    let title: String? = dictionary["title"] as? String
+                    let director: String? = dictionary["director"] as? String
+                    let release_date: String? = dictionary["release_date"] as? String
+                    if let title2:String = title,
+                        let director2:String = director, let release_date2:String = release_date {
+                        nameLabel.text = "The Star wars film \(title2), was directed by \(director2) and came out on \(release_date2)."
+                    }
+                    
+                }
+                
+            }
+            
+        }
+    }
 }
